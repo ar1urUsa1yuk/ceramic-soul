ed3c0ffdd9b8f445e69bdb36572918e320bc61db
Artur <arturusatyukl@gmail.com>
2025-04-16 03:53:50 +0300

    added section with slider, will need to be completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,39 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 
 import "../sass/style.scss";
+
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5,
+        },
+        1920: {
+            spaceBetween: 35,
+        },
+    },
+    modules: [Navigation, Pagination],
+});
+
+
+
