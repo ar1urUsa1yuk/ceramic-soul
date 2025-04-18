984f3b153bdbc80bae7f9a2d9f0faa9e97e5aba5
Artur <arturusatyukl@gmail.com>
2025-04-18 04:17:07 +0300

    added section -footer-, and 'burger' menu in header for mobile device, and all of this is adapted for mobile devices

--- src/js/script.js
+++ src/js/script.js
@@ -11,29 +11,50 @@ import "purecss/build/grids-responsive-min.css";
 import "../sass/style.scss";
 
 
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".icon-right-open",
-        prevEl: ".icon-left-open",
-    },
-    breakpoints: {
-        // when window width is >= 1200px
-        1200: {
-            slidesPerView: 3,
-            spaceBetween: 5,
-        },
-        1920: {
-            spaceBetween: 35,
-        },
-    },
-    modules: [Navigation, Pagination],
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu-active");
+    document.body.style.overflow = "hidden";
 });
 
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu-active");
+    document.body.style.owerflow = "";
+});
+
+
+
+
+
+
+
+try {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".icon-right-open",
+            prevEl: ".icon-left-open",
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+            1920: {
+                spaceBetween: 35,
+            },
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) { }
 
 
